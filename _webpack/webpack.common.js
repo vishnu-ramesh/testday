2afc9261cb331650860e2c511cd1589334597864
Vishnu Ramesh <Bablu@Vishnus-MacBook-Air.local>
2020-01-15 21:14:43 +0000

    feat: webpack

---
+++ _webpack/webpack.common.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/client/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, '../_dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
