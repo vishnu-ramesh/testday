7f693fe3beffaf47dce2d8a0c3f14f75ed9064a6
Vishnu Ramesh <Bablu@Vishnus-MacBook-Air.local>
2020-01-20 21:08:06 +0000

    feat: clean, visualizer and analyzer

--- _webpack/webpack.common.js
+++ _webpack/webpack.common.js
@@ -1,5 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const Visualizer = require('webpack-visualizer-plugin');
+
 
 module.exports = {
   entry: {
@@ -25,5 +29,17 @@ module.exports = {
       title: 'My website',
       template: path.resolve(__dirname, '../_templates/client.html'),
     }),
+
+      new CleanWebpackPlugin(),
+
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+        analyzerMode: 'static',
+        reportFilename: path.resolve(__dirname, `../_dist/bundle.html`),
+      }),
+      new Visualizer({
+        filename: './visualizer.html',
+      }),
+
   ],
 };
