f5c5f0ee06a9a865bfd5bcb02edb82bd4f867994
Vishnu Ramesh <Bablu@Vishnus-MacBook-Air.local>
2020-01-22 21:00:26 +0000

    feat: add subtract

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-class App extends Component {
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  increment = () => {
+    const currentVal = this.state.count;
+
+    this.setState({
+      // eslint-disable-next-line no-magic-numbers
+      count: currentVal + 1,
+    });
+  }
+  decrement = () => {
+    const currentVal = this.state.count;
+
+    this.setState({
+      // eslint-disable-next-line no-magic-numbers
+      count: currentVal - 1,
+    });
+  }
   render() {
-    return <h1>Hello, World</h1>;
+    return <div>
+      <p>{this.state.count}</p>
+      <button onClick={this.increment}>+</button>
+      <button onClick={this.decrement}>-</button>
+      </div>
   }
 }
 
